837b5678056efc2f02d9ec7e0dea614f37fa2771
Diego Pinzon <diego.pinzon@joinworth.com>
2026-02-12 11:44:48 -0500

    Organicing spaces

--- src/gifs/components/GifResults.tsx
+++ src/gifs/components/GifResults.tsx
@@ -1,24 +1,23 @@
 import type { FC } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 
-
 interface GifsResultsProps {
-  gifs: Gif[];
+    gifs: Gif[];
 }
 
 export const GifsResults: FC<GifsResultsProps> = ({ gifs = [] }) => {
-  return (
-    <div className="gifs-container">
-    {
-        gifs.map((gif) => (
-            <div key={gif.id} className="gif-card">
-                <img src={gif.url} alt={gif.title} width={gif.width} height={gif.height} />
-                <h3 className="montserrat-regular">{gif.title}</h3>
-                <p>
-                    {gif.width} x {gif.height} (1.5mb)
-                </p>
-            </div>
-    ))}
-</div>
-  )
+    return (
+        <div className="gifs-container">
+            {
+                gifs.map((gif) => (
+                    <div key={gif.id} className="gif-card">
+                        <img src={gif.url} alt={gif.title} width={gif.width} height={gif.height} />
+                        <h3 className="montserrat-regular">{gif.title}</h3>
+                        <p>
+                            {gif.width} x {gif.height} (1.5mb)
+                        </p>
+                    </div>
+                ))}
+        </div>
+    )
 }
